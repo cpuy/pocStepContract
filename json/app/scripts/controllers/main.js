1394744270702d747bd38b57149ad1cf36653dfe
Colin PUY <colin.puy@bonitasoft.com>
2014-09-05 14:58:03 +0200

    angular json submit ok

--- json/app/scripts/controllers/main.js
+++ json/app/scripts/controllers/main.js
@@ -8,9 +8,29 @@
  * Controller of the jsonApp
  */
 angular.module('jsonApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
+
+        $scope.lines = [{}, {}];
 
         $scope.submit = function() {
-            alert('ca marche');
+            $http.post('something/tobe/done', $scope.lines);
         }
-  });
+  })
+    .directive("fileread", [function () {
+        return {
+            scope: {
+                fileread: "="
+            },
+            link: function (scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+                        scope.$apply(function () {
+                            scope.fileread = loadEvent.target.result;
+                        });
+                    }
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        }
+    }]);
